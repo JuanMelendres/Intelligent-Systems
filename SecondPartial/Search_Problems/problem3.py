da4a30eb63aff70de68ffad0a6f60b1c1d8bd5ad
JuanMelendres <juan.mele97@gmail.com>
2020-10-12 16:43:00 -0500

    Update problem 3 maze solver
    Signed-off-by: JuanMelendres <juan.mele97@gmail.com>

--- SecondPartial/Search_Problems/problem3.py
+++ SecondPartial/Search_Problems/problem3.py
@@ -11,14 +11,12 @@ class MazeSolver(SearchProblem):
     def __init__(self, board):
         self.board = board
         self.goal = (0, 0)
-
         for y in range(len(self.board)):
             for x in range(len(self.board[y])):
                 if self.board[y][x] == "O":
                     self.initial = (x, y)
                 elif self.board[y][x] == "X":
                     self.goal = (x, y)
-
         super(MazeSolver, self).__init__(initial_state=self.initial)
 
     # Define the method that takes actions
@@ -29,13 +27,11 @@ class MazeSolver(SearchProblem):
             newx, newy = self.result(state, action)
             if self.board[newy][newx] != "+":
                 actions.append(action)
-
         return actions
 
     # Update the state based on the action
     def result(self, state, action):
         x, y = state
-
         if action.count("up"):
             y -= 1
         if action.count("down"):
@@ -44,9 +40,7 @@ class MazeSolver(SearchProblem):
             x -= 1
         if action.count("right"):
             x += 1
-
         new_state = (x, y)
-
         return new_state
 
     # Check if we have reached the goal
@@ -61,106 +55,110 @@ class MazeSolver(SearchProblem):
     def heuristic(self, state):
         x, y = state
         gx, gy = self.goal
-
         return math.sqrt((x - gx) ** 2 + (y - gy) ** 2)
 
 if __name__ == "__main__":
     # Define the map
-    MAP = """
-    ++++++++++++++++++++++
-    + O +   ++ ++        +
-    +     +     +++++++ ++
-    + +    ++  ++++ +++ ++
-    + +   + + ++         +
-    +          ++  ++  + +
-    +++++ + +      ++  + +
-    +++++ +++  + +  ++   +
-    +          + +  + +  +
-    +++++ +  + + +     X +
-    ++++++++++++++++++++++
+    MAP = ["""
+++++++++++++++++++++++
++ O +   ++ ++        +
++     +     +++++++ ++
++ +    ++  ++++ +++ ++
++ +   + + ++         +
++          ++  ++  + +
++++++ + +      ++  + +
++++++ +++  + +  ++   +
++          + +  + +  +
++++++ +  + + +     X +
+++++++++++++++++++++++
     """
 
-    '''
-    MAP = """
-    ++++++++++++++++++++++++++++++
-    + o       +              +   +
-    + ++++    ++++++++       +   +
-    +    +    +              +   +
-    +    +++     +++++  ++++++   +
-    +      +   +++   +           +
-    +      +     +   +  +  +   +++
-    +     +++++    +    +  + x   +
-    +              +       +     +
-    ++++++++++++++++++++++++++++++
+    
+    ,"""
+++++++++++++++++++++++++++++++
++ O       +              +   +
++ ++++    ++++++++       +   +
++    +    +              +   +
++    +++     +++++  ++++++   +
++      +   +++   +           +
++      +     +   +  +  +   +++
++     +++++    +    +  + X   +
++              +       +     +
+++++++++++++++++++++++++++++++
     """
 
-    MAP = """
-    ++++++++++++++++++++++++++++++
-    +         +              +   +
-    + ++++    ++++++++       +   +
-    +  o +    +              +   +
-    +    +++     +++++  ++++++   +
-    +      +   +++   +           +
-    +      +     +   +  +  +   +++
-    +     +++++    +    +  + x   +
-    +              +       +     +
-    ++++++++++++++++++++++++++++++
+ 
+    ,"""
+++++++++++++++++++++++++++++++
++         +              +   +
++ ++++    ++++++++       +   +
++  O +    +              +   +
++    +++     +++++  ++++++   +
++      +   +++   +           +
++      +     +   +  +  +   +++
++     +++++    +    +  + X   +
++              +       +     +
+++++++++++++++++++++++++++++++
     """
 
-    MAP = """
-    ++++++++++++++++++++++++++++++
-    +o                       +   +
-    +  +++    ++++++++       +   +
-    +  + +    +              +   +
-    +    +++     +++++ +++++++   +
-    +      +   ++++              +
-    +      +     + + +  +  +   +++
-    +     +++++        ++  + + + +
-    +  ++          + +          x+
-    ++++++++++++++++++++++++++++++
-    """
-'''
+    ,"""
+++++++++++++++++++++++++++++++
++O                       +   +
++  +++    ++++++++       +   +
++  + +    +              +   +
++    +++     +++++ +++++++   +
++      +   ++++              +
++      +     + + +  +  +   +++
++     +++++        ++  + + + +
++  ++          + +          X+
+++++++++++++++++++++++++++++++
+    """]
 
-    # Convert map to a list
-    print(MAP)
-    MAP = [list(x) for x in MAP.split("\n") if x]
+    for map in MAP: 
+        # Convert map to a list
+        print("------MAP------")
+        print(map)
+        map = [list(x) for x in map.split("\n") if x]
 
-    # Define cost of moving around the map
-    cost_regular = 1.0
-    cost_diagonal = 1.7
+        # Define cost of moving around the map
+        cost_regular = 1.0
+        cost_diagonal = 1.7
 
-    # Create the cost dictionary
-    COSTS = {
-        "up": cost_regular,
-        "down": cost_regular,
-        "left": cost_regular,
-        "right": cost_regular,
-        "up left": cost_diagonal,
-        "up right": cost_diagonal,
-        "down left": cost_diagonal,
-        "down right": cost_diagonal,
-    }
+        # Create the cost dictionary
+        COSTS = {
+            "up": cost_regular,
+            "down": cost_regular,
+            "left": cost_regular,
+            "right": cost_regular,
+            "up left": cost_diagonal,
+            "up right": cost_diagonal,
+            "down left": cost_diagonal,
+            "down right": cost_diagonal,
+        }
 
-    # Create maze solver object
-    problem = MazeSolver(MAP)
+        # Create maze solver object
+        problem = MazeSolver(map)
 
-    # Run the solver
-    result = astar(problem, graph_search=True)
+        # Run the solver
+        result = astar(problem, graph_search=True)
 
-    # Extract the path
-    path = [x[1] for x in result.path()]
-
-    # Print the result
-    print()
-    for y in range(len(MAP)):
-        for x in range(len(MAP[y])):
-            if (x, y) == problem.initial:
-                print('O', end='')
-            elif (x, y) == problem.goal:
-                print('X', end='')
-            elif (x, y) in path:
-                print('*', end='')
-            else:
-                print(MAP[y][x], end='')
-
+        # Extract the path
+        path = [x[1] for x in result.path()]
+        print("--Solution--")
         print()
+        # Print the steps and coordinate path as string
+        print(result.path())
+        # Print the map
+        print()
+        for y in range(len(map)):
+            for x in range(len(map[y])):
+                if (x, y) == problem.initial:
+                    print('O', end='')
+                elif (x, y) == problem.goal:
+                    print('X', end='')
+                elif (x, y) in path:
+                    print('*', end='')
+                else:
+                    print(map[y][x], end='')
+
+            print()
